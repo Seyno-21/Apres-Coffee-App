d8a8739bca72ad0690279b309af065d2886825da
Theo Taylor <theotaylornz@gmail.com>
2022-11-02 09:48:09 +1300

    Button Functionality

---
+++ Coffee-Shop-App/Coffee-Shop-App/Coffee-Shop-App/Views/MenuPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Coffee_Shop_App.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void espressoPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EspressoPage());
+        }
+
+        private async void hotDrinksPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HotDrinksPage());
+        }
+
+        private async void coldDrinksPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ColdDrinksPage());
+        }
+
+        private async void quickBitesPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new QuickBitesPage());
+        }
+    }
+}
